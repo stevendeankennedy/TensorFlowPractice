0a43fa27670ae216c6d232afeadafb5a866c0f00
Steve K <stevendeankennedy@gmail.com>
2018-01-25 14:28:22 -0500

    Adding examples for Chapters 2-3

--- Example0001.py
+++ Example0001.py
@@ -1,4 +1,7 @@
 import tensorflow as tf
+import numpy as np
+
+# variables
 
 init_val = tf.random_normal((1,5),0,1)
 var = tf.Variable(init_val, name='var')
@@ -10,3 +13,21 @@ with tf.Session() as sess:
     post_var = sess.run(var)
 
 print("\npost run: \n{}".format(post_var))
+
+# placeholders
+
+x_data = np.random.randn(5,10)
+w_data = np.random.randn(10,1)
+
+with tf.Graph().as_default():
+    x = tf.placeholder(tf.float32, shape=(5,10))
+    w = tf.placeholder(tf.float32, shape=(10,1))
+    b = tf.fill((5,1), -1.)
+    xw = tf.matmul(x,w)
+
+    xwb = xw + b
+    s = tf.reduce_max(xwb)
+    with tf.Session() as sess:
+        outs = sess.run(s, feed_dict = {x: x_data, w: w_data})
+
+print("\nouts = {}".format(outs))
